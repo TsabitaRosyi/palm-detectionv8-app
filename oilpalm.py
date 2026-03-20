d6e991a727efd5dff0741da5e301e05e815fbc61
Tsabita Rosyidah Putri <139309070+TsabitaRosyi@users.noreply.github.com>
2026-03-20 07:06:15 +0700

    Update oilpalm.py

--- oilpalm.py
+++ oilpalm.py
@@ -46,7 +46,7 @@ label_to_color = {
     "mentah": Color.BLACK
 }
 label_annotator = LabelAnnotator(
-    text_scale=3,       #  perbesar ukuran teks (default kecil)
+    text_scale=2,       #  perbesar ukuran teks (default kecil)
     text_thickness=4,     #  tebalkan huruf
     text_padding=6        #  beri jarak biar tidak mepet box
 )
